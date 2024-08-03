fa529c58dc0738cfc4f7df1adf87b50c5d8ee781
Julien <julienliang2740@gmail.com>
2024-08-03 12:15:46 -0400

    backgup

--- convert.py
+++ convert.py
@@ -3,6 +3,7 @@ from basic_pitch import ICASSP_2022_MODEL_PATH
 from basic_pitch.constants import AUDIO_SAMPLE_RATE, FFT_HOP
 from music21 import converter, instrument, note, chord, stream, environment
 
+import mido
 import librosa
 
 import pretty_midi
@@ -12,7 +13,15 @@ import numpy as np
 import os
 from pathlib import Path
 
-def wav_to_midi(input_name, input_dir, output_dir):
+def change_instrument_mido(midi_file_path, new_instrument, output_file_path, is_drum=False):
+    mid = mido.MidiFile(midi_file_path)
+    for track in mid.tracks:
+        for i, msg in enumerate(track):
+            if msg.type == 'program_change':
+                track[i] = mido.Message('program_change', program=new_instrument)
+    mid.save(output_file_path)
+
+def wav_to_midi(input_name, input_dir, output_dir, new_instrument):
     output_name_model = input_name.replace(".wav", ".npz")
     output_name_midi = input_name.replace(".wav", ".mid")
     output_name_csv = input_name.replace(".wav", ".csv")
@@ -22,9 +31,15 @@ def wav_to_midi(input_name, input_dir, output_dir):
 
     model_output, midi_data, note_events = predict(input_path)
 
-    output_path = os.path.join(output_dir, output_name_midi)
-    with open(output_path, 'wb') as f:
+    temp_midi_path = os.path.join(output_dir, 'temp_' + output_name_midi)
+    with open(temp_midi_path, 'wb') as f:
         midi_data.write(f)
+
+    output_path = os.path.join(output_dir, output_name_midi)
+    change_instrument_mido(temp_midi_path, new_instrument, output_path)
+
+    # REMOVE TEMP FILE
+    os.remove(temp_midi_path)
 
     # # Verify lengths
     # original_length = librosa.get_duration(filename=input_path)
@@ -49,18 +64,11 @@ def midi_to_pdf(input_name, input_dir, output_dir, musescore_path):
 
     # Debu print
     print(f"Checking for midi file at: {midi_path}")
-
-    # Check if file exists
     if not os.path.exists(midi_path):
         raise FileNotFoundError(f"The file {midi_path} does not exist.")
 
-    # Load midi file
     midi_data = converter.parse(midi_path)
-
-    # Analyze midi file and get parts
     parts = instrument.partitionByInstrument(midi_data)
-
-    # Create stream to hold parts
     score = stream.Score()
 
     # If multiple parts => add each part to  score
@@ -70,7 +78,6 @@ def midi_to_pdf(input_name, input_dir, output_dir, musescore_path):
     else:  # if no parts, just add the midi_data itself
         score.append(midi_data)
 
-
     score.write('musicxml', fp=output_path_musicxml)
 
     os.system(f"mscore3 {output_path_musicxml} -o {output_path_pdf}")
@@ -78,11 +85,39 @@ def midi_to_pdf(input_name, input_dir, output_dir, musescore_path):
     print(f"MusicXML file saved to: {output_path_musicxml}")
     print(f"PDF file saved to: {output_path_pdf}")
 
-def single_wav_conversion(input_name_no_filetype):
+def single_wav_conversion(input_name_no_filetype, wav_dir, midi_dir, pdf_dir, new_instrument):
     input_name_wav = input_name_no_filetype + '.wav'
     input_name_midi = input_name_no_filetype + '.mid'
-    wav_to_midi(input_name_wav, 'wav_files', 'midi_files')
-    midi_to_pdf(input_name_midi, 'midi_files', 'pdf_files', '/usr/bin/mscore3')
+    wav_to_midi(input_name_wav, wav_dir, midi_dir, new_instrument)
+    midi_to_pdf(input_name_midi, midi_dir, pdf_dir, '/usr/bin/mscore3')
+
+def batch_wav_conversion(wav_dir, midi_dir, pdf_dir):
+    for file in os.listdir(wav_dir):
+        input_name_wav = file
+        input_name_midi = file.replace('.wav', '.mid')
+
+        instrument_number = -1
+        if file == 'bass':
+            instrument_number = 1
+        elif file == 'drums':
+            instrument_number = 1
+        elif file == 'guitar':
+            instrument_number = 1
+        elif file == 'keys':
+            instrument_number = 1
+        elif file == 'piano':
+            instrument_number = 1
+        elif file == 'strings':
+            instrument_number = 1
+        elif file == 'vocals':
+            instrument_number = 1
+        elif file == 'wind':
+            instrument_number = 1
+        else: # the 'other' file
+            instrument_number = 1
+
+        wav_to_midi(input_name_wav, wav_dir, midi_dir, instrument_number)
+        midi_to_pdf(input_name_midi, midi_dir, pdf_dir, '/usr/bin/mscore3')
 
 if __name__ == "__main__":
     print("Testing module for conversion of individual .wav files")
@@ -90,4 +125,6 @@ if __name__ == "__main__":
     # wav_to_midi('xinzhengcheng_trumpet.wav', 'wav_files', 'midi_files')
     # midi_to_pdf('xinzhengcheng_trumpet.mid', 'midi_files', 'pdf_files', '/usr/bin/mscore3')
     
-    single_wav_conversion('xinzhengcheng_trumpet')
+    # single_wav_conversion('erika_trumpet', 'wav_files', 'midi_files', 'pdf_files', 57) # number corresponds to instrument in midi conversion system
+    # batch_wav_conversion('redsun_wav', 'redsun_midi', 'redsun_pdf')
+    single_wav_conversion('random_drumline', 'wav_files', 'midi_files', 'pdf_files', 128)
