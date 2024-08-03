c9a69bf0596f39100f1565b4b12368435497e522
Evan Yan <evanyans@gmail.com>
2024-08-03 01:19:11 -0400

    routing global

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import './App.css';
 import '@radix-ui/themes/styles.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Upload from './Home/Upload';
+
 
 function App() {
   return (
-	<div className="App">
-		<header className="App-header">
-			<div className='m-10'>
-				<h1 className='font-inter opacity-75 text-large font-medium mb-2'>The Best Sheet Music Generator</h1>
-				<Upload />
-			</div>
-		</header>
-	</div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <div className='m-10'>
+            <h1 className='font-inter opacity-75 text-large font-medium mb-2'>
+              The Best Sheet Music Generator
+            </h1>
+            <Routes>
+              <Route path="/upload" element={<Upload />} />
+              <Route path="/" element={<Upload />} />
+            </Routes>
+          </div>
+        </header>
+      </div>
+    </Router>
   );
 }
 
